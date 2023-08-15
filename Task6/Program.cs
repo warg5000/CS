a18ad62740a58cfe0e74e3d712232873e4a8223f
warg5000 <warg5000@gmail.com>
2023-08-15 15:13:37 +0300

    Work

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int n1 = Convert.ToInt32(Console.ReadLine());
+if (n1%2 == 0) {
+    Console.WriteLine("Число чётное");
+}
+else {
+    Console.WriteLine("Число нечётное");
+    }
