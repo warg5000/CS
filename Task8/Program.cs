70d73f41391c1874fb7f16050b33ed34d05daa21
warg5000 <warg5000@gmail.com>
2023-08-15 17:36:57 +0300

    work

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,13 +1,15 @@
 ﻿Console.WriteLine("Введите число 2 и более");
 int n = Convert.ToInt32(Console.ReadLine());
-if (n < 2) 
+if (n < 2)
 {
-    Console.WriteLine("Создание числового ряда невозможно, необходимо ввести число больше 1");
+    Console.Write("Создание числового ряда невозможно, необходимо ввести число больше 1");
 }
-string res ="";
-for (int i = 2; i <= n; i = i + 2)
+else
 {
-    res = res + $"i = {i}, ";
+    string res = "";
+    for (int i = 2; i <= n; i = i + 2)
+    {
+        res = res + $"i = {i}, ";
+    }
+    Console.Write(res.TrimEnd(' ').TrimEnd(',') + ".");
 }
-
-Console.Write(res.TrimEnd(' ').TrimEnd(',')+".");
