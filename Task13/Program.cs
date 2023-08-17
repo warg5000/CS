fb13ecd27b6de9419e16c555157578af960d9e10
warg5000 <warg5000@gmail.com>
2023-08-18 00:24:25 +0300

    Work

---
+++ Task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string? n = Console.ReadLine()?.TrimStart('-'); 
+if (n == null || n.Length <= 2)
+{
+    Console.WriteLine("Число состоит из менее 3 цифр");
+}
+else
+{
+    char res = n[2];
+    Console.WriteLine($"Третья цифра числа = {res}");
+}
