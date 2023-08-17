c43c03cbcf493126ed7eb93717c8de6f2dbcf4b2
warg5000 <warg5000@gmail.com>
2023-08-18 00:39:31 +0300

    Work

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Введите число");
-string? n = Console.ReadLine()?.TrimStart('-'); 
-if (n == null || n.Length <= 2)
+string? n = Console.ReadLine()?.TrimStart('-');
+if (n == null || !int.TryParse(n, out int garbage) || n.Length <= 2)
 {
-    Console.WriteLine("Число состоит из менее 3 цифр");
+    Console.WriteLine("Число состоит из менее 3 цифр или это не число");
 }
 else
 {
