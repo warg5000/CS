f28a154a289cfdf423ad064c1ea9c07dd0d04bc9
warg5000 <warg5000@gmail.com>
2023-10-13 19:08:06 +0300

    Work

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,19 +1,30 @@
 ﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 9
 //m = 3, n = 2 -> A(m,n) = 29
-int InputNumbers(string input)
-{
-    Console.Write(input);
-    int output = Convert.ToInt32(Console.ReadLine());
-    return output;
-}
 int Ack(int m, int n)
 {
+     if (m < 0 || n < 0)
+    {
+        throw new Exception("Ошибка, все числа должны быть натуральными");
+    }
     if (m == 0) return n + 1;
-    else if (n == 0) return Ack(m - 1, 1);
+    if (n == 0) return Ack(m - 1, 1);
     else return Ack(m - 1, Ack(m, n - 1));
 }
-int m = InputNumbers("Введите m: ");
-int n = InputNumbers("Введите n: ");
-int functionAckerman = Ack(m, n);
-Console.Write($"Функция Аккермана = {functionAckerman} ");
+try
+{
+    Console.WriteLine("Задайте первое натуральное число m");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Задайте второе натуральное число n");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int Ackerman = Ack(m, n);
+    Console.Write($"Функция Аккермана = {Ackerman} ");
+}
+catch (System.FormatException)
+{
+    Console.WriteLine("Вы не смогли ввести число");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
